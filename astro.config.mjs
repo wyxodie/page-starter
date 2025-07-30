aad4bb5bbd848c6b78bda1767d9c47fc362d0117
wyxodie <wyxodie@gmail.com>
2025-07-30 05:54:13 +0000

    Update homepage with css installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,21 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
     },
+    
   }),
 });
